f6d2dbfe7da1f6ef248f78094b87540992451f6b
rladudals1234 <goadiddl@naver.com>
2026-02-25 18:29:16 +0900

    2월25일 수업내용

---
+++ s0225_01/src/main/java/com/java/S022501Application.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S022501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S022501Application.class, args);
+	}
+
+}
---
+++ s0225_01/src/main/java/com/java/service/MemberService.java
@@ -0,0 +1,21 @@
+package com.java.service;
+
+import java.util.List;
+
+import com.java.dto.MemberDto;
+
+public interface MemberService {
+	public MemberDto findByIdAndPw(MemberDto mdto);
+
+	public MemberDto selectLogin(MemberDto mdto);
+
+	public MemberDto save(MemberDto mdto);
+
+	public List<MemberDto> findAll();
+
+	public void deleteById(MemberDto mdto);
+
+	public MemberDto findById(MemberDto mdto);
+
+	public MemberDto update(MemberDto mdto);
+}
